32ae7d7cadcd0891a20a5d5cac8c7102b09dd94c
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-01-28 17:09:56 +0100

    fix: fix typo

--- MARTe2/MARTe2_setup.sh
+++ MARTe2/MARTe2_setup.sh
@@ -1,5 +1,5 @@
 export TARGET=armv8-linux
 export MARTe2_DIR=$HOME/MARTe2
-export MARTe2_Componenets_DIR=$HOME/MARTe2-components
+export MARTe2_Components_DIR=$HOME/MARTe2-components
 export MARTe2_EXAMPLES_DIR=$HOME/MARTe2-examples
 export SDN_CORE_DIR=$HOME/SDN_1.0.12_nonCCS
