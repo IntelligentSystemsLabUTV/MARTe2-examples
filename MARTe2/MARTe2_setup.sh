82c7313c9997a52e317dfa43e4c8905eae0b0bc9
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-01-29 17:41:07 +0100

    feat: add MDSReader cfg

--- MARTe2/MARTe2_setup.sh
+++ MARTe2/MARTe2_setup.sh
@@ -1,5 +1,7 @@
 export TARGET=armv8-linux
-export MARTe2_DIR=$HOME/MARTe2
-export MARTe2_Components_DIR=$HOME/MARTe2-components
-export MARTe2_EXAMPLES_DIR=$HOME/MARTe2-examples
+export MDSPLUS_DIR=$HOME/mdsplus
 export SDN_CORE_DIR=$HOME/SDN_1.0.12_nonCCS
+export MARTe2_DIR=$HOME/MARTe2-armv8
+export MARTe2_Components_DIR=$HOME/MARTe2-components
+export MARTe2_Examples_DIR=$HOME/MARTe2-examples
+export WRRD_path=$MARTe2_Examples_DIR/MARTe2/Trees/WrRd
