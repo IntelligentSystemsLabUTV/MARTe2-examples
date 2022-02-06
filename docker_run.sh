91536f14cdc7bdcc94f056521d04cbe4b20952c9
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-02-06 11:22:20 +0100

    feat: add script to run docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [[ "$#" -ne 1 ]]; then
-    echo "Usage: docker_run <image>"
+    echo "Usage: ./docker_run <image>"
     exit 1
 fi
 
-docker run -it --priveged --network host \
+sudo docker run -it --privileged --network host \
 --env TERM=xterm-256color \
 --env DISPLAY=$DISPLAY \
 --user marte \
--v ~/.ssh:/home/$USERNAME/.ssh:ro \
--v ./config-armv8/zsh_history:/home/marte/zsh_history \
--v ./config-armv8/.aliases.sh:/home/marte/.aliases.sh \
--v ./config-armv8/.bashrc:/home/marte/.bashrc \
--v ./config-armv8/.nanorc:/home/marte/.nanorc \
--v ./config-armv8/.p10k.zsh:/home/marte/.p10k.zsh \
--v ./config-armv8/.zshrc:/home/marte/.zshrc \
--v ./:/home/marte/workspace \
+-v ~/.ssh:/home/marte/.ssh:ro \
+-v /home/pi/MARTe2-examples/config-armv8/zsh_history:/home/marte/zsh_history \
+-v /home/pi/MARTe2-examples/config-armv8/.aliases.sh:/home/marte/.aliases.sh \
+-v /home/pi/MARTe2-examples/config-armv8/.bashrc:/home/marte/.bashrc \
+-v /home/pi/MARTe2-examples/config-armv8/.nanorc:/home/marte/.nanorc \
+-v /home/pi/MARTe2-examples/config-armv8/.p10k.zsh:/home/marte/.p10k.zsh \
+-v /home/pi/MARTe2-examples/config-armv8/.zshrc:/home/marte/.zshrc \
+-v /home/pi/MARTe2-examples:/home/marte/workspace \
 $1 zsh
