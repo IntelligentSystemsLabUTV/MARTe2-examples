b640cdf32f6c446fd5854f389529efd542de2ae5
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-04-22 12:27:30 +0200

    fix: improve tree creation

--- Trees/Log/create_tree.py
+++ Trees/Log/create_tree.py
@@ -4,8 +4,7 @@ import os
 import MDSplus as mds
 
 tree_name = 'log'
-tree_path = '/home/marte2/workspace/Trees/Log/'
-os.environ[tree_name + '_path'] = tree_path
+os.environ[tree_name + '_path'] = os.path.abspath(os.getcwd())
 
 T = mds.Tree(tree_name, -1, 'NEW')
 T.addNode('COUNTER', 'SIGNAL')
