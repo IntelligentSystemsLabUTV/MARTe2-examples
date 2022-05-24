04eb780331cbb9d0ed950301618b223d81996717
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2022-05-24 14:59:43 +0000

    feat: modify RTApps startup

--- MARTe2/Startup/MATLABApp.sh
+++ MARTe2/Startup/MATLABApp.sh
@@ -8,22 +8,6 @@
 # -d cgdb = Run with cgdb
 # -d strace = Run with strace
 
-export HOME=/home/marte2
-if [[ "$(uname -m)" == "aarch64" ]]; then
-    export TARGET=armv8-linux
-    export MARTe2_DIR=$HOME/MARTe2-armv8
-elif [[ "$(uname -m)" == "x86_64" ]]; then
-    export TARGET=x86-linux
-    export MARTe2_DIR=$HOME/MARTe2
-else
-    echo "Error: $(uname -m) not supported. Architectures availables are aarch64 and x86_64"
-    exit 1
-fi
-export MARTe2_Components_DIR=$HOME/MARTe2-components
-export MARTe2_Examples_DIR=$HOME/workspace/MARTe2
-export MATLAB_Examples_DIR=$HOME/workspace/MATLAB
-export MDSPLUS_DIR=$HOME/MDSplus
-export SDN_CORE_DIR=$HOME/SDN_1.0.12_nonCCS
 export control_path=$HOME/workspace/Trees/Control
 
 # Run with cgdb or strace?
